8e183c37b4e67fc1f2c4da6b66b02a2cc8eee3ae
1Nelsonel <nelsonmokoro37@gmail.com>
2023-02-22 17:12:47 +0300

    success

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -58,7 +58,7 @@ func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 // Reservation render make reservation page and display form
 func (m *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
 	var emptyReservation models.Reservation
-	data:= make(map[string]interface{})
+	data := make(map[string]interface{})
 	data["reservation"] = emptyReservation
 	render.RenderTemplate(w, r, "make-reservation.page.tmpl", &models.TemplateData{
 		Form: forms.New(nil),
@@ -85,10 +85,9 @@ func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
 
 	form := forms.New(r.PostForm)
 
-	form.Required("first_name","last_name","email")
+	form.Required("first_name", "last_name", "email")
 	form.MinLength("first_name", 3, r)
 	form.IsEmail("email")
-	
 
 	if !form.Valid() {
 		data := make(map[string]interface{})
