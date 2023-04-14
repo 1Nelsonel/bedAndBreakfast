1ca026e46dfa20525a6f109430b913a0f073cdc2
Nelson Mokoro <nelsonmokoro37@gmail.com>
2023-04-14 11:58:58 +0300

    success

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -5,11 +5,12 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-
 	"github.com/1Nelsonel/bedAndBreakfast/internal/config"
 	"github.com/1Nelsonel/bedAndBreakfast/internal/forms"
+	"github.com/1Nelsonel/bedAndBreakfast/internal/helpers"
 	"github.com/1Nelsonel/bedAndBreakfast/internal/models"
 	"github.com/1Nelsonel/bedAndBreakfast/internal/render"
+	"github.com/pkg/errors"
 )
 
 // Repo the repository used by the handlers
@@ -34,25 +35,13 @@ func NewHandlers(r *Repository) {
 
 // Home is the handler for the home page
 func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
-	remoteIP := r.RemoteAddr
-	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
-
 	render.RenderTemplate(w, r, "home.page.tmpl", &models.TemplateData{})
 }
 
 // About is the handler for the about page
 func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
-	// perform some logic
-	stringMap := make(map[string]string)
-	stringMap["test"] = "Hello, again"
-
-	remoteIP := m.App.Session.GetString(r.Context(), "remote_ip")
-	stringMap["remote_ip"] = remoteIP
-
 	// send data to the template
-	render.RenderTemplate(w, r, "about.page.tmpl", &models.TemplateData{
-		StringMap: stringMap,
-	})
+	render.RenderTemplate(w, r, "about.page.tmpl", &models.TemplateData{})
 }
 
 // Reservation render make reservation page and display form
@@ -60,6 +49,7 @@ func (m *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
 	var emptyReservation models.Reservation
 	data := make(map[string]interface{})
 	data["reservation"] = emptyReservation
+
 	render.RenderTemplate(w, r, "make-reservation.page.tmpl", &models.TemplateData{
 		Form: forms.New(nil),
 		Data: data,
@@ -68,11 +58,10 @@ func (m *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
 
 // PostReservation handles the posting of reservation form
 func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
-	// render.RenderTemplate(w, r, "make-reservation.page.tmpl", &models.TemplateData{})
 	err := r.ParseForm()
+	err = errors.New("this is an error message")
 	if err != nil {
-		log.Println(err)
-
+		helpers.ServerError(w, err)
 		return
 	}
 
@@ -155,7 +144,7 @@ func (m *Repository) Contact(w http.ResponseWriter, r *http.Request) {
 	render.RenderTemplate(w, r, "contact.page.tmpl", &models.TemplateData{})
 }
 
-// ReservationSummary display Reservation 
+// ReservationSummary display Reservation
 func (m *Repository) ReservationSummary(w http.ResponseWriter, r *http.Request) {
 	reservation, ok := m.App.Session.Get(r.Context(), "reservation").(models.Reservation)
 	if !ok {
